56cb8bc9d0e7334c2f08304997edee7f04ecdd15
weiwenhao <weiwenhao-szgx@bestpay.com.cn>
2025-10-31 18:27:51 +0800

    update

---
+++ basic04/异常.py
@@ -0,0 +1,24 @@
+try:
+    x = int(input("请输入一个整数: "))
+    y = 10 / x
+    print("结果:", y)
+except ValueError:
+    print("输入的不是有效整数！")
+except ZeroDivisionError:
+    print("不能除以零！")
+except:
+    print("其他异常！")
+finally:
+    print("程序结束！")
+
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError("除数不能为零！")
+    return a / b
+
+
+try:
+    divide(10, 0)
+except ZeroDivisionError as e:
+    print("发生异常：", e)
